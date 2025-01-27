1ee637026f9726c19fc1326891c9536fbe475d38
Emelie <emelievons@gmail.com>
2025-01-27 16:50:05 +0100

    Add cors dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import axios from "axios";
+import {useEffect} from "react";
 
 function App() {
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      console.log(response.data);
+    })
+  }, [])
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App"></div>
   );
 }
 
