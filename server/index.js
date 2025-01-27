89351c586271b086c1e53c4b5ee536778f3efa46
Emelie <emelievons@gmail.com>
2025-01-27 16:50:25 +0100

    Switch server port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 const db = require('./models');
 
@@ -14,8 +16,8 @@ db.sequelize.sync().then(()=>{
         res.send('Hello World!');
     });
     
-    app.listen(3000, () => {
-        console.log("Server running on port 3000");
+    app.listen(3001, () => {
+        console.log("Server running on port 3001");
     });
 });
 
