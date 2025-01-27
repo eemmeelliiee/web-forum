cadfcd5d2fe7b6bae7524edb94a8a9e551d8328b
Emelie <emelievons@gmail.com>
2025-01-27 17:11:47 +0100

    Add post listing functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import axios from "axios";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [listOfPosts, setListOfPost] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((response) => {
-      console.log(response.data);
-    })
-  }, [])
+      setListOfPost(response.data);
+    });
+  }, []);
   return (
-    <div className="App"></div>
+    <div className="App">
+      {listOfPosts.map((value, key)=>{
+      return ( 
+        <div className="post"> 
+          <div className="title">{value.title} </div> 
+          <div className="body">{value.postText}</div>
+          <div className="footer">{value.username}</div>
+        </div>
+      );
+    })}
+    </div>
   );
 }
 
